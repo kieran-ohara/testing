40201e492bd95ad2edb1a4112079e0d15b11ffd4
Kieran Bamforth <hello@kieranbamforth.me>
2022-06-13 11:09:05 +0100

    testing.

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref } from 'vue'
+
+const step = ref('postcode')
+
+const setStep = () => {
+  step.value = 'service';
+}
+
+const submitApp = async (formData) => {
+  console.log(formData);
+}
+
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+</style>
+
+<template>
+  <FormKit
+    type="form"
+    #default="{ value }"
+    @submit="submitApp"
+  >
+    <div class="form-body">
+
+      <Transition>
+        <section v-show="step === 'postcode'">
+          <h3>What is your business address</h3>
+          <FormKit
+            type="group"
+            name="postcode"
+          >
+            <FormKit
+              type="text"
+              validation="required|length:3"
+              name="postcode"
+            />
+            <FormKit
+              type="button"
+              @click="setStep"
+              label="Next"
+              name="service"
+            />
+          </FormKit>
+        </section>
+      </Transition>
+
+      <Transition>
+        <section v-show="step === 'service'">
+          <h3>Which service can we help you with?</h3>
+          <FormKit
+            type="group"
+            name="service"
+          >
+            <FormKit
+              type="radio"
+              name="service"
+              :options="['Broadband and Landline', 'Broadband only', 'Landline only']"
+            />
+          </FormKit>
+        </section>
+      </Transition>
+
+      <details>
+        <summary>Form data</summary>
+        <pre>{{ value }}</pre>
+      </details>
+
+    </div>
+  </FormKit>
+</template>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { plugin, defaultConfig } from '@formkit/vue'
+
+createApp(App).use(plugin, defaultConfig).mount('#app')
